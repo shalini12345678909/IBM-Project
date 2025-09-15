464ab87a3d711e2519094dfed2937afa52b213f3
shalini12345678909 <23ucs594@avasctnj.edu.in>
2025-09-15 10:50:02 +0530

    Create Readme.m

---
+++ Readme.m
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="refresh" content="0; url="https://huggingface.co/spaces/V-06/aicitizen" />
+  <title>Citizen-AI</title>
+</head>
+<body>
+  <h1>Redirecting to Citizen-AI...</h1>
+  <p>If you are not redirected automatically, 
+     <a href="https://huggingface.co/spaces/V-06/aicitizen">click here</a>.
+  </p>
+</body>
+</html>
