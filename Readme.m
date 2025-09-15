dc9b1d8b6b70349e066b0128659430f2d0646d06
shalini12345678909 <23ucs594@avasctnj.edu.in>
2025-09-15 20:57:10 +0530

    Update Readme.m

--- Readme.m
+++ Readme.m
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="refresh" content="0; url="https://huggingface.co/spaces/V-06/aicitizen" />
+  <meta http-equiv="refresh" content="0; url="https://huggingface.co/spaces/shalini2006/citizenai" />
   <title>Citizen-AI</title>
 </head>
 <body>
   <h1>Redirecting to Citizen-AI...</h1>
   <p>If you are not redirected automatically, 
-     <a href="https://huggingface.co/spaces/V-06/aicitizen">click here</a>.
+     <a href="https://huggingface.co/spaces/shalini2006/citizenai">click here</a>.
   </p>
 </body>
 </html>
